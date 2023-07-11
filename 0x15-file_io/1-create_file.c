6b9d4c2765ca4afa4c6b6af50ee37abd94f93d23
Mohameddwedar94 <Mohameddwedar1994@gmail.com>
2023-07-10 18:39:30 -0700

    task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,41 +1,32 @@
 #include "main.h"
 /**
- * _strlen - returns the lenth of a string
- * @s: the string whose lenth to check 
+ * create_file -creates an array of chars, and initializes
  *
- *Return: integer lenght of string
- */
-int _strlen(char *s)
-{	
-	int i = 0;
-	
-	if (!s)
-		return (0);
-	
-		while (*s++)
-			i++;
-		return (i);
-}
-
-/**
- * create_file - creates a file 
- * @filename: name of file to create
- * @text_content: text to write
+ * @text_content: is a NULL terminated string to write to the file
+ * @filename: is the name of the file to create
  *
- * Return: 1 on success 0 on failure
+ * Return: 1 on success, -1 on failure
  */
-int creat_file(const char *filename, char *text_content);
+int create_file(const char *filename, char *text_content)
 {
-	int fd;
-	ssize_t bytes = 0, len = _strlen(text_content);
-	
-	if (!filename)
+	int m, d, len = 0;
+
+	if (filename == NULL)
 		return (-1);
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUN, S_IRUSR | S_TWUSR);
-	if (fd == -1)
-		return (-1)
-	if (len)
-		bytes = write(fd, text_content, len);
-	close(fd);
-	return (bytes == len ? 1 : -1);
+
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
+
+	m = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	d = write(m, text_content, len);
+
+	if (m == -1 || d == -1)
+		return (-1);
+
+	close(m);
+
+	return (1);
 }
