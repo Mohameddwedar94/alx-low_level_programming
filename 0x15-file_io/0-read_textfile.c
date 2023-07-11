719804f8756707e25f1801ef311f7c0e41a8814a
Mohameddwedar94 <Mohameddwedar1994@gmail.com>
2023-07-10 18:18:58 -0700

    task 1

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * read_textfile | reads text from a file and print it
+ * read_textfile | reads text from a file and prints it
  * @filename: name of file to read
  * @letters: number of bytes to read
  *
@@ -15,7 +15,6 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	if (!filename || !letters)
 		return (0);
-	
 	fd = open(filename, o_RDONLY);
 	if (fd == -1)
 		return (0);
