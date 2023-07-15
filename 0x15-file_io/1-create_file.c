6e4b61c51d73c92d7102b2262b5540751684e377
Mohameddwedar94 <Mohameddwedar1994@gmail.com>
2023-07-14 18:42:20 -0700

    task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * create_file -creates an array of chars, and initializes
  *
@@ -8,28 +7,26 @@
  *
  * Return: 1 on success, -1 on failure
  */
-
-
 int create_file(const char *filename, char *text_content)
 {
-           int m, d, f = 0;
+	int m, d, f = 0;
 
-	       if (filename == NULL)
-	                return (-1);
+	if (filename == NULL)
+		return (-1);
 
-	       if (text_content != NULL)
-	     {
-		   for (f = 0; text_content[f];)
-		         f++;
-	     }
+	if (text_content != NULL)
+	{
+		for (f = 0; text_content[f];)
+			f++;
+	}
 
-	     m = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	     d = write(m, text_content, f);
+	m = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	d = write(m, text_content, f);
 
-	       if (m == -1 || d == -1)
-	                return (-1);
+	if (m == -1 || d == -1)
+		return (-1);
 
-	       close(m);
+	close(m);
 
-	       return (1);
+	return (1);
 }
