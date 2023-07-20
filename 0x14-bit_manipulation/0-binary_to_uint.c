b749a54c7125122009ca46cc4c21fc68b02e086b
Mohameddwedar94 <Mohameddwedar1994@gmail.com>
2023-07-20 08:37:28 -0700

    task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -6,23 +6,23 @@
  *
  * Return:converted number, or 0
  */
-unsigned int binary_to_uint(const char *m)
+unsigned int binary_to_uint(const char *b)
 {
 	int d;
 	unsigned int f;
 
 	f = 0;
-	if (!m)
+	if (!b)
 		return (0);
-	for (d = 0; m[d] != '\0'; d++)
+	for (d = 0; b[d] != '\0'; d++)
 	{
-		if (m[d] != '0' && m[d] != '1')
+		if (b[d] != '0' && b[d] != '1')
 			return (0);
 	}
-	for (d = 0; m[d] != '\0'; d++)
+	for (d = 0; b[d] != '\0'; d++)
 	{
 		f <<= 1;
-		if (m[d] == '1')
+		if (b[d] == '1')
 			f += 1;
 	}
 	return (f);
