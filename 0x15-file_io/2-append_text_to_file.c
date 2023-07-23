eae00e22d0faeca59142a2f4e2198db0b92ab3b2
Mohameddwedar94 <Mohameddwedar1994@gmail.com>
2023-07-23 13:53:19 -0700

    task2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,40 +1,40 @@
 #include "main.h"
 
 /**
- * _strlen - finds the length of a string
- * @str: pointer to the string
+ * _strlen - find the length of a string
+ * @str: pointer to a string
  *
- * Return: length of the string
+ * Return: length of a string
  */
-size_t _strlen(char *dwe)
+size_t _strlen(char *str)
 {
 	size_t m;
 
-	for (m = 0; dwe[m]; m++)
+	for (m = 0; str[m]; m++)
 		;
 	return (m);
 }
 
 /**
- * append_text_to_file - appends a text at the end of a file.
- * @filename: name of the file
+ * append_text_to_file - appends a text at the end of the file.
+ * @filename: name of  file
  * @text_content: NULL terminated string to add at the end of the file
  *
  * Return: 1 on success and -1 on failure
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int mado;
+	int madof;
 	ssize_t len;
 
 	if (filename == NULL)
 		return (-1);
-	mado = open(filename, O_WRONLY | O_APPEND);
-	if (mado == -1)
+	madof = open(filename, O_WRONLY | O_APPEND);
+	if (madof == -1)
 		return (-1);
 	if (text_content != NULL)
-		len = write(mado, text_content, _strlen(text_content));
-	close(mado);
+		len = write(madof, text_content, _strlen(text_content));
+	close(madof);
 	if (len == -1)
 		return (-1);
 	return (1);
